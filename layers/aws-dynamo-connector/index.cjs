719a9d3d5827c50f34a660b7fc14b415214aed4b
Artume Kolesnikov <Artuem@New.Comp>
2023-10-13 13:21:12 +0300

    Dynamo settings

--- layers/aws-dynamo-connector/index.cjs
+++ layers/aws-dynamo-connector/index.cjs
@@ -1,4 +1,10 @@
 const AWS = require('aws-sdk');
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const dynamoDB = new AWS.DynamoDB.DocumentClient({
+    region: process.env.REGION, 
+    credentials: {
+      accessKeyId: process.env.ACCESS_KEY_ID, 
+      secretAccessKey: process.env.SECRET_ACCESS_KEY 
+    }
+  });
 
 module.exports = { dynamoDB };

e329be16bf4d43aafb6aa23574325efc91eb009a
Artume Kolesnikov <Artuem@New.Comp>
2023-10-13 18:51:23 +0300

    Layer cleanup

--- layers/aws-dynamo-connector/index.js
+++ layers/aws-dynamo-connector/index.js
@@ -2,13 +2,15 @@ const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
 const { DynamoDBDocumentClient } = require('@aws-sdk/lib-dynamodb');
 
 // Load credentials from environment variables
-const dynamoDBClient = new DynamoDBClient({
+const draft_dynamoDBClient = new DynamoDBClient({
     region: process.env.REGION,
     credentials: {
         accessKeyId: process.env.ACCESS_KEY_ID,
         secretAccessKey: process.env.SECRET_ACCESS_KEY
     }
 });
+
+const dynamoDBClient = new DynamoDBClient();
 
 const DocumentClient = DynamoDBDocumentClient.from(dynamoDBClient);
 

80c0b947d2c69aaba9bb8084433f3cf3bd564701
Artume Kolesnikov <Artuem@New.Comp>
2023-10-13 15:15:00 +0300

    No ENV

--- layers/aws-dynamo-connector/index.cjs
+++ layers/aws-dynamo-connector/index.cjs
@@ -1,6 +1,5 @@
 const DynamoDBClient = require('@aws-sdk/client-dynamodb')
 const DynamoDBDocumentClient = require('@aws-sdk/lib-dynamodb')
-const fromEnv = require('@aws-sdk/credential-provider-env')
 
 console.log('REGION:', process.env.REGION);
 console.log('ACCESS_KEY_ID:', process.env.ACCESS_KEY_ID);
@@ -13,12 +12,12 @@ console.log('ACCESS_KEY_ID:', process.env.AWS_ACCESS_KEY_ID);
 console.log('SECRET_ACCESS_KEY:', process.env.AWS_SECRET_ACCESS_KEY);
 
 // Load credentials from environment variables
-const credentials = fromEnv();
-
-// Set the region and credentials when initializing the DynamoDBClient
 const dynamoDBClient = new DynamoDBClient({
-  region: process.env.AWS_REGION, // AWS region from environment variables
-  credentials: credentials,
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
 });
 
 // Initialize the DocumentClient using the DynamoDBClient instance

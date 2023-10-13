5d5888506e14bc9f5fdddb59f1dd9991c76f9950
Artume Kolesnikov <Artuem@New.Comp>
2023-10-13 19:39:41 +0300

    No region try

--- layers/aws-dynamo-connector/index.js
+++ layers/aws-dynamo-connector/index.js
@@ -1,7 +1,7 @@
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
 const { DynamoDBDocumentClient } = require('@aws-sdk/lib-dynamodb');
 
-const dynamoDBClient = new DynamoDBClient({region: process.env.AWS_REGION});
+const dynamoDBClient = new DynamoDBClient();
 console.log(dynamoDBClient)
 
 const DocumentClient = DynamoDBDocumentClient.from(dynamoDBClient);

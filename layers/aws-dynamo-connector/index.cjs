08d26ec753afc7ea5bd52efa5470171cf23c24c3
Artume Kolesnikov <Artuem@New.Comp>
2023-10-13 13:48:51 +0300

    config change

--- layers/aws-dynamo-connector/index.cjs
+++ layers/aws-dynamo-connector/index.cjs
@@ -4,12 +4,14 @@ console.log('REGION:', process.env.REGION);
 console.log('ACCESS_KEY_ID:', process.env.ACCESS_KEY_ID);
 console.log('SECRET_ACCESS_KEY:', process.env.SECRET_ACCESS_KEY);
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient({
+AWS.config.update({
     region: process.env.REGION, 
     credentials: {
       accessKeyId: process.env.ACCESS_KEY_ID, 
       secretAccessKey: process.env.SECRET_ACCESS_KEY 
     }
-  });
+});
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 module.exports = { dynamoDB };

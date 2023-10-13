03742d4a99d06e3aedf9438cc9dfbec6d4fb440f
Artume Kolesnikov <Artuem@New.Comp>
2023-10-13 13:32:22 +0300

    debug

--- layers/aws-dynamo-connector/index.cjs
+++ layers/aws-dynamo-connector/index.cjs
@@ -1,4 +1,9 @@
 const AWS = require('aws-sdk');
+
+console.log('REGION:', process.env.REGION);
+console.log('ACCESS_KEY_ID:', process.env.ACCESS_KEY_ID);
+console.log('SECRET_ACCESS_KEY:', process.env.SECRET_ACCESS_KEY);
+
 const dynamoDB = new AWS.DynamoDB.DocumentClient({
     region: process.env.REGION, 
     credentials: {

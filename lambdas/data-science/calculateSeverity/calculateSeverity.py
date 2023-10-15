013e58f3b34f32863825288dd95c1ba81e200c19
IdoLior99 <35223557+IdoLior99@users.noreply.github.com>
2023-10-15 16:51:15 +0300

    Updated formula

--- lambdas/data-science/calculateSeverity/calculateSeverity.py
+++ lambdas/data-science/calculateSeverity/calculateSeverity.py
@@ -20,7 +20,7 @@ def severity(text):
     sentiment = testimonial.sentiment
     # Normalize Polarity and weigh in objectivity to create a 0-1 scale measure
     # Higher score = a more severe, objective report
-    sev = 0.8 * (-1 * sentiment.polarity - (-1) / (1 - (-1))) + 0.2 * (1 - sentiment.subjectivity)
+    sev = 0.8 * ((-1 * sentiment.polarity - (-1)) / 2) + 0.2 * (1 - sentiment.subjectivity)
     return sev
 
 
@@ -50,15 +50,7 @@ def lambda_handler(event, context):
 
 
 # Expected Input Format ######
-# event = {
-#     "text": "Reporting from the heart of Canada, I stand amidst the aftermath of a devastating earthquake that \
-# has sent shockwaves through the nation. The scene before me is one of chaos and destruction, a stark reminder of \
-# the unforgiving power of nature. Buildings once standing tall now lie in ruins, their structural integrity compromised \
-# by the violent tremors. Streets are cracked, and debris is strewn across the landscape, painting a grim picture of the \
-# widespread havoc wreaked by this seismic event. The local population's resilience is evident as they sift through the rubble, \
-# searching for survivors and tending to the injured. Emergency responders work tirelessly, battling against the odds to \
-# provide aid and relief to those affected by this catastrophe. As the dust settles, the magnitude of the situation becomes \
-# all too clear – this earthquake has left a deep scar on the Canadian landscape, testing the resolve of its people as they \
-# unite in the face of adversity."
-# }
-# lambda_handler(event, {})
+event = {
+    "text": "This is very bad."
+}
+lambda_handler(event, {})

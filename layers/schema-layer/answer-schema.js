a13bb5f6d48a0264bef8ede105bf6f91c65c340b
Artume Kolesnikov <Artuem@New.Comp>
2023-10-16 10:27:39 +0300

    Refactor rna

--- layers/schema-layer/answer-schema.js
+++ layers/schema-layer/answer-schema.js
@@ -23,38 +23,35 @@ const tableDefinition = {
   };
 
 
-const createTable = async () => {
-    try {
-      await DynamoDB.createTable(tableDefinition).promise();
-      console.log("Table created successfully.");
-    } catch (error) {
-      console.error("Error creating table:", error);
-    }
-  };
-  
-  createTable(); 
-  
-  
+  const createTable = () => {
+    return DynamoDB.createTable(tableDefinition).promise()
+        .then(() => {
+            console.log("Table created successfully.");
+            const AnswerTable = new Table({
+                ...dynamoSdkToToolbox(tableDefinition),
+                DocumentClient
+            });
 
-const AnswerTable = new Table({
-    ...dynamoSdkToToolbox(tableDefinition),
-    DocumentClient
-})
+            const Answer = new Entity({
+                name: 'Answer',
+                table: AnswerTable,
+                attributes: {
+                    id: { type: 'string', partitionKey: true, default: () => uuidv4() }, 
+                    questionId: 'string',
+                    rnaId: 'string', 
+                    value: 'map',
+                    photos: { type: 'list', required: false }, 
+                    notes: { type: 'string', required: false },
+                    creationDate: { type: 'string', default: () => new Date().toISOString() }
+                }
+            });
 
+            module.exports = { Answer };
+        })
+        .catch(error => {
+            console.error("Error creating table:", error);
+            throw error; // Rethrow the error to be caught by the caller
+        });
+};
 
-const Answer = new Entity({
-    name: 'Answer',
-    table: AnswerTable,
-    attributes: {
-        id: { type: 'string', partitionKey: true, default: () => uuidv4() }, 
-        questionId: 'string',
-        rnaId: 'string', 
-        value: 'map',
-        photos: { type: 'list', required: false }, 
-        notes: { type: 'string', required: false },
-        creationDate: { type: 'string', default: () => new Date().toISOString() }
-    }
-});
-
-
-module.exports = { Answer };
+createTable();
--- layers/schema-layer/rna-schema.js
+++ layers/schema-layer/rna-schema.js
@@ -1,25 +1,56 @@
 const { Table, Entity } = require('dynamodb-toolbox');
 const { DocumentClient } = require('/opt/aws-dynamo-connector/index.js');
 const { v4: uuidv4 } = require('uuid');
+const AWS = require("aws-sdk");
+const { dynamoSdkToToolbox } = require("/opt/dynamoSdkToToolbox/index.js");
+
+const DynamoDB = new AWS.DynamoDB();
+const tableDefinition = {
+    TableName: "Rnas",
+    AttributeDefinitions: [
+      {
+        AttributeType: "S",
+        AttributeName: "id",
+      }
+    ],
+    KeySchema: [
+      {
+        AttributeName: "id",
+        KeyType: "HASH",
+      }
+    ],
+    BillingMode: "PAY_PER_REQUEST"
+  };
 
 
-const RnaTable = new Table({
-    name: "Rnas",
-    partitionKey: 'id',
-    DocumentClient: DocumentClient
-});
+  const createTable = () => {
+    return DynamoDB.createTable(tableDefinition).promise()
+        .then(() => {
+            console.log("Table created successfully.");
+            const RnaTable = new Table({
+                ...dynamoSdkToToolbox(tableDefinition),
+                DocumentClient
+            });
 
-const RNA = new Entity({
-    name: 'RNA',
-    table: RnaTable,
-    attributes: {
-        id: { type: 'string', partitionKey: true, default: () => uuidv4() }, 
-        isCompleted: 'boolean',
-        communityName: 'string',
-        communityType: 'string',
-        location: 'string',
-        creationDate: { type: 'string', default: () => new Date().toISOString() }
-    }
-});
+            const RNA = new Entity({
+                name: 'RNA',
+                table: RnaTable,
+                attributes: {
+                    id: { type: 'string', partitionKey: true, default: () => uuidv4() }, 
+                    isCompleted: 'boolean',
+                    communityName: 'string',
+                    communityType: 'string',
+                    location: 'string',
+                    creationDate: { type: 'string', default: () => new Date().toISOString() }
+                }
+            });
 
-module.exports = { RNA };
+            module.exports = { RNA };
+        })
+        .catch(error => {
+            console.error("Error creating table:", error);
+            throw error; // Rethrow the error to be caught by the caller
+        });
+};
+
+createTable();

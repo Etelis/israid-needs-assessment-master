6bdef460ae5b9e4f93c88f5907af2c677c1aaff4
Artume Kolesnikov <Artuem@New.Comp>
2023-10-16 10:37:51 +0300

    Redo

--- layers/schema-layer/rna-schema.js
+++ layers/schema-layer/rna-schema.js
@@ -23,29 +23,10 @@ const tableDefinition = {
   };
 
 
-  const createTable = () => {
+const createTable = async () => {
     return DynamoDB.createTable(tableDefinition).promise()
         .then(() => {
             console.log("Table created successfully.");
-            const RnaTable = new Table({
-                ...dynamoSdkToToolbox(tableDefinition),
-                DocumentClient
-            });
-
-            const RNA = new Entity({
-                name: 'RNA',
-                table: RnaTable,
-                attributes: {
-                    id: { type: 'string', partitionKey: true, default: () => uuidv4() }, 
-                    isCompleted: 'boolean',
-                    communityName: 'string',
-                    communityType: 'string',
-                    location: 'string',
-                    creationDate: { type: 'string', default: () => new Date().toISOString() }
-                }
-            });
-
-            module.exports = { RNA };
         })
         .catch(error => {
             console.error("Error creating table:", error);
@@ -53,4 +34,29 @@ const tableDefinition = {
         });
 };
 
-createTable();
+
+async function setupEntities() {
+    const tableDefinition = await createTable();
+
+    const RnaTable = new Table({
+        ...dynamoSdkToToolbox(tableDefinition),
+        DocumentClient
+    });
+
+    const RNA = new Entity({
+        name: 'RNA',
+        table: RnaTable,
+        attributes: {
+            id: { type: 'string', partitionKey: true, default: () => uuidv4() }, 
+            isCompleted: 'boolean',
+            communityName: 'string',
+            communityType: 'string',
+            location: 'string',
+            creationDate: { type: 'string', default: () => new Date().toISOString() }
+        }
+    });
+
+    module.exports = { RNA };
+}
+
+setupEntities();

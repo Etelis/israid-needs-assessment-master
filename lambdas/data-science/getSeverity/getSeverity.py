179fd22913a61ecd1e66bbf20d5dc391371f7a2c
amitbb310 <benbasat.am@gmail.com>
2023-10-24 10:05:09 +0300

    changed

--- lambdas/data-science/getSeverity/getSeverity.py
+++ lambdas/data-science/getSeverity/getSeverity.py
@@ -9,6 +9,36 @@ import math
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+example_event_allrna = {
+    'fb91fdc7-46ba-47f8-bc3c-d1e480ad71be': {   # first RNA ID - can be the ONLY ONE
+        '550e8400-e29b-41d4-a716-446655440000': {
+            'value': 'No',
+            'notes': 'There are no routes to the schools because some cars are blocking the way'
+        },
+        '550e8400-e29b-41d4-a716-446655440001': {
+            'value': 'The elder leader says the community never used toilets and do not know the concept of sanitation',
+            'notes': ''
+        },
+        '550e8400-e29b-41d4-a716-446655440002': {
+            'value': 'Yes',
+            'notes': ''
+        }
+    },
+    'fb91fdc7-46ba-47f8-bc3c-d1e480ad71bf': {   # second RNA ID - this wont exist incase of 1 RNA
+        'c7013a49-a854-4266-aa12-8de91f22ca31': {
+            'value': 'Yes',
+            'notes': ''
+        },
+        'c7013a49-a854-4266-aa12-8de91f22ca32': {
+            'value': 'There are several other groups in the area like: NGA, VTYA, and the local government',
+            'notes': ''
+        },
+        'c7013a49-a854-4266-aa12-8de91f22ca33': {
+            'value': 'Yes',
+            'notes': ''
+        }
+    }
+}
 
 def severity(text):
     """
@@ -46,12 +76,11 @@ def get_severity(answers):
     severity_dict = {}
     for item in answers:
         total_grade = 0.0
-        for answer in item:
-            total_grade += calaculate_answer(answer)
+        for answer in answers[item]:
+            total_grade += calaculate_answer(answers[item][answer])
         severity_dict[item] = total_grade/len(item)
     severity_dict = normalize(severity_dict)
     return severity_dict
-
 
 
 def lambda_handler(event, context):
@@ -77,6 +106,8 @@ def lambda_handler(event, context):
             'body': json.dumps({'error_message': 'Internal server error'})
         }
 
+
+print(get_severity(example_event_allrna))
 
 # Expected Input Format ######
 #event = { 

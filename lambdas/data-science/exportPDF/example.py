f47da00b2cbd2e05c41e4e7b81534d722a6a18d3
Eyal Huri <eyalhuri7@Gmail.com>
2023-10-25 15:17:03 +0300

    added example to exportPDF

---
+++ lambdas/data-science/exportPDF/example.py
@@ -0,0 +1,111 @@
+# event = 
+# {
+# 'rna.id':
+#     {
+#     "Creator",
+#     "CreatorPosition",
+#     "CreatorPhone",
+#     "Emergency",
+#     "AffectedHouseholds",
+#     "communityName",
+#     "communityType",
+#     "location",
+#     "createdOn",
+#     "lastUpdatedOn",
+#     "isCompleted"
+#     },
+# 'rna_questions': 
+#     [
+#       {'question.id':
+#           "question.category",
+#           "question.subCategory",
+#           "question.question",
+#           "question.order",
+#           "question.options"
+#        },
+#  ...],
+# 'rna_answers': 
+#     [
+#       {'answer.id':
+#             "answer.questionId",
+#             "answer.value",
+#             "answer.photos",
+#             "answer.notes"
+#        },
+#  ...],
+# 'categories': 
+#    [
+#       {'category.id':
+#             "category.name"
+#       },
+#  ...],
+# "subcategories": {
+#   [
+#       {"subcategory.id":
+#             "subcategory.name",
+#             "subcategory.categoryId"
+#        },
+#  ...]
+# }
+
+example_event_allrna = {
+    'fb91fdc7-46ba-47f8-bc3c-d1e480ad71be': {   # first RNA ID - can be the ONLY ONE
+        '550e8400-e29b-41d4-a716-446655440000': {
+            'value': False,
+            'notes': 'There are no routes to the schools because some cars are blocking the way'
+        },
+        '550e8400-e29b-41d4-a716-446655440001': {
+            'value': 'The elder leader says the community never used toilets and do not know the concept of sanitation',
+            'notes': ''
+        },
+        '550e8400-e29b-41d4-a716-446655440002': {
+            'value': True,
+            'notes': ''
+        }
+    },
+    'fb91fdc7-46ba-47f8-bc3c-d1e480ad71bf': {   # second RNA ID - this wont exist incase of 1 RNA
+        'c7013a49-a854-4266-aa12-8de91f22ca31': {
+            'value': True,
+            'notes': ''
+        },
+        'c7013a49-a854-4266-aa12-8de91f22ca32': {
+            'value': 'There are several other groups in the area like: NGA, VTYA, and the local government',
+            'notes': ''
+        },
+        'c7013a49-a854-4266-aa12-8de91f22ca33': {
+            'value': True,
+            'notes': ''
+        }
+    }
+}
+
+example_event_categories = {
+    'fb91fdc7-46ba-47f8-bc3c-d1e480ad71be': {   # first category ID - can be the ONLY ONE
+        '550e8400-e29b-41d4-a716-446655440000': {
+            'value': False,
+            'notes': 'There are no routes to the schools because some cars are blocking the way'
+        },
+        '550e8400-e29b-41d4-a716-446655440001': {
+            'value': 'The elder leader says the community never used toilets and do not know the concept of sanitation',
+            'notes': ''
+        },
+        '550e8400-e29b-41d4-a716-446655440002': {
+            'value': True,
+            'notes': ''
+        }
+    },
+    'fb91fdc7-46ba-47f8-bc3c-d1e480ad71bf': {   # second category ID - this wont exist incase of 1 category
+        'c7013a49-a854-4266-aa12-8de91f22ca31': {
+            'value': True,
+            'notes': ''
+        },
+        'c7013a49-a854-4266-aa12-8de91f22ca32': {
+            'value': 'There are several other groups in the area like: NGA, VTYA, and the local government',
+            'notes': ''
+        },
+        'c7013a49-a854-4266-aa12-8de91f22ca33': {
+            'value': True,
+            'notes': ''
+        }
+    }
+}

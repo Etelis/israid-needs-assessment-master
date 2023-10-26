3770287eb6bbfa58a17c1fb2646bb8e97ac26313
Tecington1 <turtcbui123@gmail.com>
2023-10-26 21:11:19 +0300

    refactor: moved remote url to enviorment variable

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const api = axios.create({
 	baseURL:
 		import.meta.env.VITE_NODE_ENV === 'production'
-			? 'https://dom6b8ltd7.execute-api.eu-north-1.amazonaws.com/IsraAid'
+			? import.meta.env.VITE_API_URL
 			: 'http://localhost:3000/api',
 	headers: {
 		'Content-type': 'application/json',

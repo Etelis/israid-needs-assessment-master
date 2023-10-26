9fbcbd290d19ea94ce6e0ac0d7369afafbbcb033
Artume Kolesnikov <Artuem@New.Comp>
2023-10-26 12:40:48 +0300

    Settle createdon

--- lambdas/synchronization/synchronize/index.js
+++ lambdas/synchronization/synchronize/index.js
@@ -1,121 +1,127 @@
-const { RNA } = require('/opt/schema-layer/rna-schema.js');
-const { Answer } = require('/opt/schema-layer/answer-schema.js');
+const { RNA } = require("/opt/schema-layer/rna-schema.js");
+const { Answer } = require("/opt/schema-layer/answer-schema.js");
 
 const hasRnaChanged = (oldRna, newRna) => {
-	if (!newRna) {
-		return false;
-	}
+  if (!newRna) {
+    return false;
+  }
 
-	return (
-		newRna.communityName !== oldRna.communityName ||
-		newRna.location !== oldRna.location ||
-		newRna.isCompleted !== oldRna.isCompleted ||
-		newRna.communityType !== oldRna.communityType ||
-		newRna.affectedHouseholds !== oldRna.affectedHouseholds
-	);
+  return (
+    newRna.communityName !== oldRna.communityName ||
+    newRna.location !== oldRna.location ||
+    newRna.isCompleted !== oldRna.isCompleted ||
+    newRna.communityType !== oldRna.communityType ||
+    newRna.affectedHouseholds !== oldRna.affectedHouseholds
+  );
 };
 
 const getUpdatedRnasModels = (oldRnas, newRnas) =>
-	newRnas
-		.map((newRna) => {
-			const oldRna = oldRnas.find((x) => x.id === newRna.id);
+  newRnas
+    .map((newRna) => {
+      const oldRna = oldRnas.find((x) => x.id === newRna.id);
 
-			if (!oldRna) {
-				return RNA.put(newRna);
-			}
+      if (!oldRna) {
+        return RNA.put(newRna);
+      }
 
-			if (!hasRnaChanged(oldRna, newRna)) {
-				return null;
-			}
+      if (!hasRnaChanged(oldRna, newRna)) {
+        return null;
+      }
 
-			oldRna.communityName = newRna.communityName;
-			oldRna.communityType = newRna.communityType;
-			oldRna.location = newRna.location;
-			oldRna.affectedHouseholds = newRna.affectedHouseholds;
-			oldRna.lastUpdatedOn = new Date().toISOString();
-			oldRna.isCompleted = newRna.isCompleted;
+      oldRna.communityName = newRna.communityName;
+      oldRna.communityType = newRna.communityType;
+      oldRna.location = newRna.location;
+      oldRna.affectedHouseholds = newRna.affectedHouseholds;
+      oldRna.lastUpdatedOn = new Date().toISOString();
+      oldRna.isCompleted = newRna.isCompleted;
 
-			return RNA.update({ PK: oldRna.id }, { ...oldRna });
-		})
-		.filter((x) => x);
+      return RNA.update({ PK: oldRna.id }, { ...oldRna });
+    })
+    .filter((x) => x);
 
 const hasAnswerChanged = (oldAnswer, newAnswer) => {
-	if (!newAnswer) {
-		return false;
-	}
+  if (!newAnswer) {
+    return false;
+  }
 
-	return (
-		newAnswer.value !== oldAnswer.value ||
-		JSON.stringify(newAnswer.photos) !== JSON.stringify(oldAnswer.photos) ||
-		newAnswer.notes !== oldAnswer.notes
-	);
+  return (
+    newAnswer.value !== oldAnswer.value ||
+    JSON.stringify(newAnswer.photos) !== JSON.stringify(oldAnswer.photos) ||
+    newAnswer.notes !== oldAnswer.notes
+  );
 };
 
 const getUpdatedAnswersModels = (oldAnswers, newAnswers) =>
-	newAnswers
-		.map((newAnswer) => {
-			const oldAnswer = oldAnswers.find(
-				(x) =>
-					x.rnaId === newAnswer.rnaId &&
-					x.questionId === newAnswer.questionId
-			);
+  newAnswers
+    .map((newAnswer) => {
+      const oldAnswer = oldAnswers.find(
+        (x) =>
+          x.rnaId === newAnswer.rnaId && x.questionId === newAnswer.questionId
+      );
 
-			if (!oldAnswer) {
-				return Answer.put(newAnswer);
-			}
+      if (!oldAnswer) {
+        return Answer.put(newAnswer);
+      }
 
-			if (
-				!hasAnswerChanged(oldAnswer, newAnswer) ||
-				new Date(oldAnswer.createdOn) > new Date(newAnswer.createdOn)
-			) {
-				return null;
-			}
+      if (
+        !hasAnswerChanged(oldAnswer, newAnswer) ||
+        new Date(oldAnswer.createdOn) > new Date(newAnswer.createdOn)
+      ) {
+        return null;
+      }
 
-			return Answer.update({ PK: oldAnswer.id }, { ...newAnswer });
-		})
-		.filter((x) => x);
+      return Answer.update({ PK: oldAnswer.id }, { ...newAnswer });
+    })
+    .filter((x) => x);
 
 exports.handler = async (event) => {
-	try {
-		const { updatedRnas, updatedAnswers } = event;
+  try {
+    const { updatedRnas, updatedAnswers } = event;
 
-		const formattedUpdatedRnas = updatedRnas.map((rna) => ({
-			...rna,
-			createdOn: rna.createdOn.toISOString(),
-			lastUpdatedOn: rna.lastUpdatedOn
-				? rna.lastUpdatedOn.toISOString()
-				: null,
-		}));
+    const formattedUpdatedRnas = updatedRnas.map((rna) => {
+      const formattedRna = {
+        ...rna,
+        lastUpdatedOn: rna.lastUpdatedOn
+          ? rna.lastUpdatedOn.toISOString()
+          : null,
+      };
 
-		const formattedUpdatedAnswers = updatedAnswers.map((answer) => ({
-			...answer,
-			createdOn: answer.createdOn.toISOString(),
-		}));
+      if (rna.createdOn !== null) {
+        formattedRna.createdOn = rna.createdOn.toISOString();
+      }
 
-		const updatedRnasModels = getUpdatedRnasModels(
-			(await RNA.scan()).Items,
-			formattedUpdatedRnas
-		);
+      return formattedRna;
+    });
 
-		const updatedAnswersModels = getUpdatedAnswersModels(
-			(await Answer.scan()).Items,
-			formattedUpdatedAnswers
-		);
+    const formattedUpdatedAnswers = updatedAnswers.map((answer) => ({
+      ...answer,
+      createdOn: answer.createdOn.toISOString(),
+    }));
 
-		await Promise.all(updatedRnasModels, updatedAnswersModels);
+    const updatedRnasModels = getUpdatedRnasModels(
+      (await RNA.scan()).Items,
+      formattedUpdatedRnas
+    );
 
-		return {
-			statusCode: 200,
-		};
-	} catch (error) {
-		console.error(error);
+    const updatedAnswersModels = getUpdatedAnswersModels(
+      (await Answer.scan()).Items,
+      formattedUpdatedAnswers
+    );
 
-		return {
-			statusCode: 500,
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ message: 'Internal Server Error' }),
-		};
-	}
+    await Promise.all(updatedRnasModels, updatedAnswersModels);
+
+    return {
+      statusCode: 200,
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: "Internal Server Error" }),
+    };
+  }
 };

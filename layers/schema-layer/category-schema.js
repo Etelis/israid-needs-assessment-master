72894df69692f0286b85ac609adca15f3ed8e433
Tecington <82104730+Tecington@users.noreply.github.com>
2023-11-10 11:31:08 +0200

    removed category schema

--- layers/schema-layer/category-schema.js
+++ layers/schema-layer/category-schema.js
@@ -4,52 +4,52 @@ const { v4: uuidv4 } = require('uuid');
 const AWS = require('aws-sdk');
 const { dynamoSdkToToolbox } = require('/opt/dynamoSdkToToolbox/index.js');
 
-const DynamoDB = new AWS.DynamoDB();
-const tableDefinition = {
-	TableName: 'Categories',
-	AttributeDefinitions: [
-		{
-			AttributeType: 'S',
-			AttributeName: 'id',
-		},
-	],
-	KeySchema: [
-		{
-			AttributeName: 'id',
-			KeyType: 'HASH',
-		},
-	],
-	BillingMode: 'PAY_PER_REQUEST',
-};
+// const DynamoDB = new AWS.DynamoDB();
+// const tableDefinition = {
+// 	TableName: 'Categories',
+// 	AttributeDefinitions: [
+// 		{
+// 			AttributeType: 'S',
+// 			AttributeName: 'id',
+// 		},
+// 	],
+// 	KeySchema: [
+// 		{
+// 			AttributeName: 'id',
+// 			KeyType: 'HASH',
+// 		},
+// 	],
+// 	BillingMode: 'PAY_PER_REQUEST',
+// };
 
-const createTable = async () => {
-	try {
-		await DynamoDB.createTable(tableDefinition).promise();
-		console.log('Table created successfully.');
-	} catch (error) {
-		console.error('Error creating table:', error);
-	}
-};
+// const createTable = async () => {
+// 	try {
+// 		await DynamoDB.createTable(tableDefinition).promise();
+// 		console.log('Table created successfully.');
+// 	} catch (error) {
+// 		console.error('Error creating table:', error);
+// 	}
+// };
 
-createTable();
+// createTable();
 
-const CategoriesTable = new Table({
-	...dynamoSdkToToolbox(tableDefinition),
-	DocumentClient,
-});
+// const CategoriesTable = new Table({
+// 	...dynamoSdkToToolbox(tableDefinition),
+// 	DocumentClient,
+// });
 
-const Category = new Entity({
-	name: 'Category',
-	table: CategoriesTable,
-	attributes: {
-		id: { type: 'string', partitionKey: true, default: () => uuidv4() },
-		name: 'string',
-		description: 'string',
-		iconSrc: 'string',
-		subCategories: {
-			type: 'list'
-		},
-	},
-});
+// const Category = new Entity({
+// 	name: 'Category',
+// 	table: CategoriesTable,
+// 	attributes: {
+// 		id: { type: 'string', partitionKey: true, default: () => uuidv4() },
+// 		name: 'string',
+// 		description: 'string',
+// 		iconSrc: 'string',
+// 		subCategories: {
+// 			type: 'list'
+// 		},
+// 	},
+// });
 
-module.exports = { Category };
+// module.exports = { Category };
